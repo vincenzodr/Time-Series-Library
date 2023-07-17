564d6743d033844ca489392098e345142e03873d
Vincenzo De Rosa <58515721+vincenzodr@users.noreply.github.com>
2023-07-17 21:10:06 +0200

    Moved GPVS data loader in data_loader.py

--- data_provider/data_factory.py
+++ data_provider/data_factory.py
@@ -1,6 +1,5 @@
 from data_provider.data_loader import Dataset_ETT_hour, Dataset_ETT_minute, Dataset_Custom, Dataset_M4, PSMSegLoader, \
-    MSLSegLoader, SMAPSegLoader, SMDSegLoader, SWATSegLoader, UEAloader
-from data_provider.GPVS_loader import GPVSLoader
+    MSLSegLoader, SMAPSegLoader, SMDSegLoader, SWATSegLoader, UEAloader, GPVSLoader
 from data_provider.uea import collate_fn
 from torch.utils.data import DataLoader
 
